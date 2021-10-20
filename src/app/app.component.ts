b660623a40f5b97b620e207df8c9cac1f0df2d7a
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 08:23:17 -0500

    final log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,6 @@ import { Component } from '@angular/core';
   // styleUrls: ['./app.component.css']
   // now with backticks - whichever works for you
   styles: [`
-      h3{
-        color: dodgerblue;
-      };
 
       .online{
         color: white;
@@ -24,15 +21,15 @@ export class AppComponent {
   afterFiveClicks = false;
 
   onShow(){
-    console.log('does this appear?');
     this.clicks.push(this.clicks.length + 1);
     this.showText = !this.showText;
     console.log(this.clicks);
   };
 
   afterFive(){
-    // here I messed up - its telling it for every item inside of the array when it should be only after 4
-    // replace the code inside of the app.html to fix so that only the first 4 are not blue
+    // here is my inital solution to the problem 
+    // I messed up - its telling it for every item INSIDE of the array when it should be only after 4
+    // inline coded inside of the app.html to fix it so that only the first 4 are not blue but 5 and up are 
     // NOTE: this function is no longer needed
     this.afterFiveClicks = true;
     return this.clicks.length >= 5 ? 'blue' : 'transparent';
